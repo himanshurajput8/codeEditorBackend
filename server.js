3b15ee714057670941858ef77a211dd7afd6586b
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-05-21 13:57:21 +0530

    first commit

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://code-editor-five-alpha.vercel.app',
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 const server = http.createServer(app);
+
 
 const io = new Server(server, {
   cors: {
-    origin: '*', //  frontend origin
+    // origin: '*', 
+    origin: 'https://code-editor-five-alpha.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true
   },
 });
 
