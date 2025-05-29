64bf452c2438854764aad4aedee94d98d90b5399
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-05-29 10:40:36 +0530

    update uii

--- sockets/codeSocket.js
+++ sockets/codeSocket.js
@@ -1,21 +1,31 @@
-const { v4: uuidv4 } = require('uuid');
-
+// sockets/codeSocket.js
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log('New client connected:', socket.id);
+    console.log(`Socket connected: ${socket.id}`);
 
     socket.on('join-room', (roomId) => {
       socket.join(roomId);
-      console.log(`${socket.id} joined room ${roomId}`);
+      console.log(`${socket.id} joined room: ${roomId}`);
     });
 
     socket.on('code-change', ({ roomId, data }) => {
-      // Broadcast to everyone in the room except sender
       socket.to(roomId).emit('code-update', data);
     });
 
+    socket.on('cursor-position-change', ({ roomId, lineNumber, column }) => {
+socket.to(roomId).emit('cursor-position-update', { lineNumber, column ,senderId: socket.id});
+    });
+
+    socket.on('selection-change', ({ roomId, selection }) => {
+      console.log('Selected');
+      socket.to(roomId).emit('selection-update', {
+        senderId: socket.id,
+        selection,
+      });
+    });
+
     socket.on('disconnect', () => {
-      console.log('Client disconnected:', socket.id);
+      console.log(`Socket disconnected: ${socket.id}`);
     });
   });
 };
