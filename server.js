b0d303172d0b8fc022baa8ea6b3ca762d124f5b0
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-06-02 11:29:18 +0530

    change port no

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
   res.send('CodeShare backend running...');
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
     console.log('running');
     
